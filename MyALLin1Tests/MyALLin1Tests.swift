9a91ce8ad949a3d142e732b0159b97a4f666cf2c
Alex Mills <alexanderwmills@gmail.com>
2020-03-22 13:31:21 +1100

    Initial Commit

---
+++ MyALLin1Tests/MyALLin1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MyALLin1Tests.swift
+//  MyALLin1Tests
+//
+//  Created by Alex Mills on 22/3/20.
+//  Copyright © 2020 MyALLin1. All rights reserved.
+//
+
+import XCTest
+@testable import MyALLin1
+
+class MyALLin1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
