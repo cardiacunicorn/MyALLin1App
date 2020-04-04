489660799e4599791c86e14c4503ffd97c195a9f
Alex Mills <alexanderwmills@gmail.com>
2020-04-04 16:47:55 +1100

    Removing Facebook Pods & functions

--- MyALLin1/View Controller/LoginViewController.swift
+++ MyALLin1/View Controller/LoginViewController.swift
@@ -7,32 +7,15 @@
 //
 
 import UIKit
-import FacebookLogin
-import FBSDKCoreKit
 
 class LoginViewController: UIViewController {
 
     @IBAction func loginButton(_ sender: Any) {
-        let loginManager = LoginManager()
-        loginManager.logIn(
-            permissions: [.publicProfile, .userFriends],
-            viewController: self
-        ) { result in
-            self.loginManagerDidComplete(result)
-        }
+        
     }
     
     @IBAction func logoutButton(_ sender: Any) {
-        let loginManager = LoginManager()
-        loginManager.logOut()
-
-        let alertController = UIAlertController(
-            title: "Logout",
-            message: "Logged out.",
-            preferredStyle: .alert
-        )
-        alertController.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alertController, animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
@@ -40,40 +23,6 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Login view loaded.")
         
-        // This is how you would check that a user is in a logged in state:
-        if let accessToken = AccessToken.current {
-            // User is logged in, use 'accessToken' here.
-            print("User is successfully logged in through Facebook.\nAccess token: \(accessToken)")
-        }
-        
-    }
-    
-    func loginManagerDidComplete(_ result: LoginResult) {
-        let alertController: UIAlertController
-        switch result {
-        case .cancelled:
-            alertController = UIAlertController(
-                title: "Login Cancelled",
-                message: "User cancelled login.",
-                preferredStyle: .alert
-            )
-
-        case .failed(let error):
-            alertController = UIAlertController(
-                title: "Login Fail",
-                message: "Login failed with error \(error)",
-                preferredStyle: .alert
-            )
-
-        case .success(let grantedPermissions, _, _):
-            alertController = UIAlertController(
-                title: "Login Success",
-                message: "Login succeeded with granted permissions: \(grantedPermissions)",
-                preferredStyle: .alert
-            )
-        }
-        alertController.addAction(UIAlertAction(title: "OK", style: .default))
-        self.present(alertController, animated: true, completion: nil)
     }
 }
 
--- Podfile
+++ Podfile
@@ -6,8 +6,6 @@ target 'MyALLin1' do
   use_frameworks!
 
   # Pods for MyALLin1
-  pod 'FacebookCore'
-  pod 'FacebookLogin'
 
   target 'MyALLin1Tests' do
     inherit! :search_paths
