e5d2b7e91ed61f0a54289c5162ba6620260f22b9
Alex Mills <alexanderwmills@gmail.com>
2020-04-18 17:03:51 +1000

    Twitter login appears to be working, redirecting to app
    Issue: success code will not run, because it terminates on opening up Safari, this is verified by the 'Get Timeline' button

--- MyALLin1/Application/AppDelegate.swift
+++ MyALLin1/Application/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Swifter
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if let callback = URL(string: "MyALLin1://") {
+            Swifter.handleOpenURL(url, callbackURL: callback)
+        }
+        
         return true
     }
 
--- MyALLin1/View Controller/LoginViewController.swift
+++ MyALLin1/View Controller/LoginViewController.swift
@@ -8,63 +8,85 @@
 
 import UIKit
 import STTwitter
+import Swifter
 
 class LoginViewController: UIViewController {
+    
+    let swifter = Swifter(consumerKey: "0bxAILPpJ4gORixVzWJfahjRV", consumerSecret: "zaDny7HAqqirRrFvrQ6SBq0s9eCuYTBAcBRKrMjqR2UmNXEz5G")
+    
+    let twitter = STTwitterAPI(oAuthConsumerKey: "0bxAILPpJ4gORixVzWJfahjRV", consumerSecret: "zaDny7HAqqirRrFvrQ6SBq0s9eCuYTBAcBRKrMjqR2UmNXEz5G")
+    
+    let alexstwitter = STTwitterAPI(oAuthConsumerKey: "0bxAILPpJ4gORixVzWJfahjRV", consumerSecret: "zaDny7HAqqirRrFvrQ6SBq0s9eCuYTBAcBRKrMjqR2UmNXEz5G", oauthToken: "2849820361-VDYgYPW59D62Eyt3DtzMGJJdSEr1WLPGKCyoTS2", oauthTokenSecret: "QieOqVMKm7KR8IXAwpcWn9XI7hFDZ7CgcdZTtptQyb1eR")
     
     // MARK: - Buttons
     
     // MARK: Login Button
     @IBAction func loginButton(_ sender: Any) {
         
+        // MARK: Swifter Login
+        if let landingURL = URL(string: "MyALLin1://") {
+            swifter.authorize(
+                withCallback: landingURL,
+                presentingFrom: self,
+                success: {
+                    (token, response) in
+                    // NOTE: The below will not print (and any other code will not run) as iOS 13 won't allow the code to run in the background without proper permissions
+                    print("You are now authorised through Twitter")
+                },
+                failure: {
+                    (error) in
+                    print("Error in authorisation process")
+                }
+            )
+        }
     }
     
     // MARK: Logout button
     @IBAction func logoutButton(_ sender: Any) {
-        
+        swifter.getHomeTimeline(count: 5, success: { (json) in
+            print(json)
+        }) { (error) in
+            print(error)
+        }
     }
     
     // MARK: - Functions
     // MARK: ViewController Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         print("Login view loaded.")
-        
-        // MARK: STTwitter Instantiation
-        // Not sure if this is just going to authorise me, or each user?
-        let twitter = STTwitterAPI(oAuthConsumerKey: "0bxAILPpJ4gORixVzWJfahjRV", consumerSecret: "zaDny7HAqqirRrFvrQ6SBq0s9eCuYTBAcBRKrMjqR2UmNXEz5G", oauthToken: "2849820361-VDYgYPW59D62Eyt3DtzMGJJdSEr1WLPGKCyoTS2", oauthTokenSecret: "QieOqVMKm7KR8IXAwpcWn9XI7hFDZ7CgcdZTtptQyb1eR")
-        
+
         // MARK: STTwitter Login
-        twitter?.verifyCredentials(
-            userSuccessBlock: { // What to do if credentials are verified
-                (username, userId) -> Void in
-                print(username ?? "No username", userId ?? "No user ID")
-                
-                // MARK: Get Timeline
-                twitter?.getHomeTimeline(
-                    sinceID: nil,
-                    count: 2, // How many statuses will be returned
-                    successBlock: { // What to do with retrieved timeline statuses
-                        (statuses) -> Void in
-                        
-                        for status in statuses! {
-                            print(status)
-                            // TODO: Process status JSON
-                        }
-                        
-                    },
-                    errorBlock: {
-                        (error) -> Void in
-                        print(error as Any)
-                    }
-                )
-                
-            },
-            errorBlock: {
-                (error) -> Void in
-                print(error as Any)
-            }
-        )
+//        alexstwitter?.verifyCredentials(
+//            userSuccessBlock: { // What to do if credentials are verified
+//                (username, userId) -> Void in
+//                print(username ?? "No username", userId ?? "No user ID")
+//
+//                // MARK: Get Timeline
+//                alexstwitter?.getHomeTimeline(
+//                    sinceID: nil,
+//                    count: 1, // How many statuses will be returned
+//                    successBlock: { // What to do with retrieved timeline statuses
+//                        (statuses) -> Void in
+//
+//                        for status in statuses! {
+//                            print(status)
+//                            // TODO: Process status JSON
+//                        }
+//
+//                    },
+//                    errorBlock: {
+//                        (error) -> Void in
+//                        print(error as Any)
+//                    }
+//                )
+//
+//            },
+//            errorBlock: {
+//                (error) -> Void in
+//                print(error as Any)
+//            }
+//        )
         
     }
 }
