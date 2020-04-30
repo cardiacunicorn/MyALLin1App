24d2ebe5457140f559fbb5326c034234d8229cf4
Alex Mills <alexanderwmills@gmail.com>
2020-04-30 16:32:31 +1000

    Removed STTwitter Pod

--- Pods/Target Support Files/Pods-MyALLin1-MyALLin1UITests/Pods-MyALLin1-MyALLin1UITests-frameworks.sh
+++ Pods/Target Support Files/Pods-MyALLin1-MyALLin1UITests/Pods-MyALLin1-MyALLin1UITests-frameworks.sh
@@ -197,11 +197,9 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/STTwitter/STTwitter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swifter/Swifter.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/STTwitter/STTwitter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swifter/Swifter.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
